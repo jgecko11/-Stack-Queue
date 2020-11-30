9f8622c1d9ce1c9a5201755b6fc87f68608cd674
Henry Johnson <46233024+HunkyHank1@users.noreply.github.com>
2020-11-30 09:39:57 -0700

    I have added peek and pop

--- src/StackQueue.java
+++ src/StackQueue.java
@@ -1,8 +1,26 @@
-public class StackQueue {
+public class StackQueue<T> {
 
     Stack Stackenque = new Stack();
     Stack Stackdeque = new Stack();
 
-
-
+    public StackNode peekQueue(){
+        Stack copyEnque = Stackenque;
+        for(int i = 0;i<Stackenque.size();i++){
+            Stackdeque.push(copyEnque.pop());
+        }
+        StackNode returnable =  Stackdeque.peek();
+        return returnable;
+    }
+    public StackNode popQueue(){
+        int size = Stackenque.size();
+        for(int i = 0;i<size;i++){
+            Stackdeque.push(Stackenque.pop());
+        }
+        StackNode returnable =  Stackdeque.pop();
+        Stack copyDeque = Stackdeque;
+        for(int i = 0;i<Stackdeque.size();i++){
+            Stackenque.push(copyDeque.pop());
+        }
+        return returnable;
+    }
 }
